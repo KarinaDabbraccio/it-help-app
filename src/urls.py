7f84efcef96108d213d70d0e3364ed55f7876ffb
dltcoughlin <75563084+dltcoughlin@users.noreply.github.com>
2022-04-13 23:30:13 -0400

    Added new ticket url

--- src/urls.py
+++ src/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from src.views import home
+from src.views import newTicket
 
 urlpatterns = [
-    path('', home.as_view, name='home')
+    path('', home.as_view, name='home'),
+    path('', newTicket.as_view, name='newticket')
 ]
