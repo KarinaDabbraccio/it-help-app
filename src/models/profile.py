846e397b6f07fca8db2b987b11556ab45d33e12e
Jared Rice <riceusaf@gmail.com>
2022-04-16 21:48:44 +0900

    Added comments for models

--- src/models/profile.py
+++ src/models/profile.py
@@ -5,14 +5,18 @@ from django.dispatch import receiver
 from .ticket import Ticket
 
 class Profile(models.Model):
-
+    '''
+    Choices for 'user_group' to identify users as 
+    either your basic user or tech that accepts tickets
+    '''
     USER_TYPE = [
         ('U', 'User'),
         ('T', 'Tech')
     ]
 
     '''
-    Extends Django's default user.model    
+    Profile Model
+    Extends Django's default user.model using one-to-one 
     Fields that are included:
 
     first_name: blank=true, 150 char
@@ -31,11 +35,21 @@ class Profile(models.Model):
     '''
     username = models.OneToOneField(User, on_delete=models.CASCADE)
 
+
     '''
     Custom fields for user.profile model in this app. 
+
+    user_group: choice from USER_TYPE to identify if user is a User or Tech
+    user_ticket: many-to-many, many 'users' can have many 'tickets'
+                 A User creates a ticket under their name, then one or multiple
+                 Techs can assign themselves to a single ticket.
+                 Users/Tech should be able to see what tickets they have 
+                 created/assigned to.
+                 blank=true because a new user may not have any tickets yet
     '''
     user_group = models.CharField(max_length=1, choices=USER_TYPE)
     user_ticket = models.ManyToManyField(Ticket, blank=True)
+
 
     '''
     This updates default user model with our profile extention
@@ -49,5 +63,8 @@ class Profile(models.Model):
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
 
+    '''
+    Displays info in admin page
+    '''
     def __str__(self) -> str:
         return '{username} - {user_type}'.format(username=self.username, user_type=self.user_group)
--- src/models/ticket.py
+++ src/models/ticket.py
@@ -1,16 +1,39 @@
 from django.db import models
 
-
 class Ticket(models.Model):
+    '''
+    Choices for the current status of tickets on 'status'
+    '''
     STATUS_TYPE = [
         ('O', 'Open'),
         ('C', 'Closed')
     ]
+
+    '''
+    Choices for the current priority of tickets on 'priority'
+    '''
     PRIOIRTY_TYPE = [
         ('R', 'Routine'),
         ('U', 'Urgent'),
         ('E', 'Emergency')
     ]
+
+    '''
+    Ticket Model
+
+    ticketNum: the id and primary key.  Auto generated as an integer
+    is_assigned:  a boolean to identify if a tech has been assgined or not
+                  default is False
+    status: choice from STATUS_TYPE to identify if ticket is open or closed
+            default is 'O' for Open
+    priority: choice from PRIOIRTY_TYPE to identify the urgency of the ticket
+    title: max length 50, short description of issue
+    description: Longer text field to describe issue
+    date_created: auto generated with current date/time
+    last_checked: blank=true, shows when tech last reviewed
+    due_date = blank=true, shows due date based off priority
+    date_closed = blank=true, shows date/time when closed
+    '''
     ticketNum = models.AutoField(auto_created=True, primary_key=True)
     is_assigned = models.BooleanField(default=False)
     status = models.CharField(max_length=1, choices=STATUS_TYPE, default='O')
@@ -22,5 +45,8 @@ class Ticket(models.Model):
     due_date = models.DateTimeField(null=True, blank=True)
     date_closed = models.DateTimeField(null=True, blank=True)
 
+    '''
+    Displays info in admin page
+    '''
     def __str__(self) -> str:
         return 'Ticket: {ticketNum} - {title}'.format(ticketNum=self.ticketNum, title=self.title)
