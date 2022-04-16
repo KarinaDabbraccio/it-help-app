c86aa1a6b176ae81dcb583192c065bcd31c7a7dc
dltcoughlin <75563084+dltcoughlin@users.noreply.github.com>
2022-04-15 23:13:23 -0400

    search ticket url

--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from src.views import home
-from src.views import newTicket
+from src.views import home, searchticket, newTicket
 
 urlpatterns = [
     path('', home.as_view, name='home'),
-    path('', newTicket.as_view, name='newticket')
+    path('', newTicket.as_view, name='newticket'),
+    path('', searchticket.as_view, name='searchticket')
 ]
