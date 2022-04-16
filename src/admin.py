cb9f05ea500b030880d1926ce8f5bdc06ff6dfa8
dltcoughlin <75563084+dltcoughlin@users.noreply.github.com>
2022-04-16 18:00:29 -0400

    Update for UserTicket

--- src/admin.py
+++ src/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 
 from src.models import Ticket
 from src.models import Comment
-from src.models import Profile
+from src.models import UserTicket
 
 # Register your models here.
 admin.site.register(Ticket)
-admin.site.register(Comment)
+admin.site.register(UserTicket)
 admin.site.register(Profile)
 
