23ae6c7bc1955d7e948aa70a2a1383cbae4dc948
Jared <riceusaf@gmail.com>
2022-04-17 12:20:05 +0900

    Revert "Update for UserTicket"
    This reverts commit cb9f05ea500b030880d1926ce8f5bdc06ff6dfa8.

--- src/admin.py
+++ src/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 
 from src.models import Ticket
 from src.models import Comment
-from src.models import UserTicket
+from src.models import Profile
 
 # Register your models here.
 admin.site.register(Ticket)
-admin.site.register(UserTicket)
 admin.site.register(Comment)
+admin.site.register(Profile)
 
