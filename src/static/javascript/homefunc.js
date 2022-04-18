a4f92a34f0631b1acee7ebb6421943cff579995c
Jared Rice <riceusaf@gmail.com>
2022-04-18 11:59:39 +0900

    Shows submited user, assigned tech and comments

--- src/static/javascript/homefunc.js
+++ src/static/javascript/homefunc.js
@@ -16,9 +16,11 @@ function getTicketInfo(ticket){
           let ticketTable = document.getElementById("ticketDiv");
           let jsonReturn = JSON.parse(response);
           ticketTable.innerHTML = "";
+          var commentCount = 0;
+          var techAssigned = false;
 
           /*
-          Since we are only passing 1 ticket with the possibily of multiple comments,
+          Since we are only passing 1 ticket with the possibily of multiple users and comments,
           we know the first object in jsonReturn will be the ticket
           */
           var obj = jsonReturn[0];
@@ -30,28 +32,43 @@ function getTicketInfo(ticket){
           ticketTable.innerHTML += "<p>Last Checked: " + obj.last_checked + "</p>";
           ticketTable.innerHTML += "<p>Date Closed: " + obj.date_closed + "</p>";
           ticketTable.innerHTML += "<p>Description: " + obj.description + "</p>";
-          ticketTable.innerHTML += "<p>Comments:</p>";
+          techAssigned = obj.is_assigned;
+          
+          /*
+          We know the second object will be the user who submitted the ticket
+          */
+          var obj = jsonReturn[1];
+          ticketTable.innerHTML += "<p>Submitted by: " + obj.username_id__username +  " (" + obj.user_group + ")</p>";
 
           /*
-          Check to see if ticket has any comments.  If not offer to add one here?
+          Loop through any remaining jsonReturn objects.  We know the third querty set contains
+          1 or more techs assigned to the ticket.  We will pull these out first then comments
           */
-          if (jsonReturn.length <= 1) {
-            ticketTable.innerHTML += "<p>There are no comments! </p>";
-          }
-
-          /*
-          Loop through any remaining jsonReturn objects, these will be comments if any
-          and print out those fields
-          */
-         else{
-          for(var i = 1; i < jsonReturn.length; i++) {
+          for(var i = 2; i < jsonReturn.length; i++) {
             var obj = jsonReturn[i];
-            ticketTable.innerHTML += "<p>Comment "+ i + "</p>";
-            ticketTable.innerHTML += "<p>Message: " + obj.message + "</p>";
-            ticketTable.innerHTML += "<p>From User: " + obj.user__username +  " (" + obj.user__profile__user_group + ")</p>";
-            ticketTable.innerHTML += "<p>Date Entered: " + obj.date_entered + "</p>";
+            // Grabs the techs from the json
+            if(!obj.message) {
+              ticketTable.innerHTML += "<p>Assigned Tech: " + obj.username_id__username +  " (" + obj.user_group + ")</p>";
+            }
+            // Grabs the comments from the json
+            else {
+              commentCount++
+              ticketTable.innerHTML += "<p>Comment "+ commentCount + "</p>";
+              ticketTable.innerHTML += "<p>Message: " + obj.message + "</p>";
+              ticketTable.innerHTML += "<p>From : " + obj.user__username +  " (" + obj.user__profile__user_group + ")</p>";
+              ticketTable.innerHTML += "<p>Date Entered: " + obj.date_entered + "</p>";
+            }
           }
-         }
+
+          //Lets user know if no tech is assigned to ticket
+          if (!techAssigned){
+            ticketTable.innerHTML += "<p>There is no tech assigned to this ticket</p>";
+          }
+
+          //Lets user know there are no comments
+          if (commentCount ==0){
+            ticketTable.innerHTML += "<p>There are no comments! Would you like to add one?</p>";
+          }
         }
       }
   )
--- src/views/getticketinfo.py
+++ src/views/getticketinfo.py
@@ -1,3 +1,4 @@
+from textwrap import indent
 from src.models import *
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
@@ -8,20 +9,27 @@ from django.forms.models import model_to_dict
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 from itertools import chain
+from django.core import serializers
 
 
 '''
 ticketResult get all the values for the ticket clicked
-commetResults gets the values for username, user_group, message and date entered
-result joins the 2 querysets together
+userResult gets the user who created this ticket
+techResults gets one or more, if any techs are assigned to this ticket
+commetResults gets the values for message, username, user_group, user_group and date entered
+        sorted by date entered for better readability 
+result joins the querysets together
 '''
 @login_required(login_url='/login')
 def as_view(request):
         if request.method == 'POST':
-                jsonReturn = json.dumps("wow");
                 ticketResult = Ticket.objects.filter(ticketNum=request.POST["ticketNum"])
+                userResult = Profile.objects.filter(user_ticket=request.POST["ticketNum"]).filter(user_group='U')
+                techResult = Profile.objects.filter(user_ticket=request.POST["ticketNum"]).filter(user_group='T')
                 commentResult = Comment.objects.filter(ticketNum=request.POST["ticketNum"]).order_by('date_entered')
-                result = chain(ticketResult.values(), commentResult.values('message', 'user__username', 'user__profile__user_group', 'date_entered'))
-                jsonReturn = json.dumps(list(result), indent = 4, sort_keys = True, default = str)
-                print(jsonReturn)
+                result = chain(ticketResult.values(),
+                        userResult.values('username_id__username', 'user_group'),
+                        techResult.values('username_id__username', 'user_group'),
+                        commentResult.values('message', 'user__username', 'user__profile__user_group', 'date_entered'))      
+                jsonReturn = json.dumps(list(result), indent = 2, default = str)
                 return JsonResponse(jsonReturn, safe=False)
